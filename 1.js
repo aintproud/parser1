94a18e2fc3c68329cce294d3fa6d13c4eef30afd
aintproud <iks.mr2016@yandex.ru>
2022-09-27 19:32:53 +1000

    k

--- 1.js
+++ 1.js
@@ -1,14 +1,41 @@
-const puppeteer = require( 'puppeteer');
-const selector = '#content > div > div.mainTabs > div.selector > div > div > a.realty';
+// const puppeteer = require( 'puppeteer');
+// const selector = '#content > div > div.mainTabs > div.selector > div > div > a.realty';
 
-(async () => {
-    const browser = await puppeteer.launch({headless: 0});
-    const page = await browser.newPage()
-    await page.setViewport({width: 1000, height: 500})
-    await page.goto('https://www.farpost.ru/vladivostok/')
-    await page.waitForSelector(selector)
+// (async () => {
+//     const browser = await puppeteer.launch({headless: 0});
+//     const page = await browser.newPage()
+//     await page.setViewport({width: 1000, height: 500})
+//     await page.goto('https://www.farpost.ru/vladivostok/')
+//     await page.waitForSelector(selector)
 
-    const e = await page.$(selector)
-    await e.click()
+//     const e = await page.$(selector)
+//     await e.click()
+// }
+// )()
+
+
+
+
+
+
+
+
+function manager(i){
+    const literalObject = {
+        [1]: 'Артем (Фрунзе)',
+        [2]: 'Биробиджан (Пионерская)',
+        [3]: 'Благовещенск (Воронкова)',
+        [4]: 'Благовещенск (Театральная)',
+        [5]: 'Владивосток (Военное Шоссе)',
+        [6]: 'Владивосток (Камская)',
+        [7]: 'Владивосток (Кубанская)',
+        [8]: 'Комсомольск-на-Амуре (Кирова)',
+        [9]: 'Находка (Вторая)',
+        [10]: 'Уссурийск (Чичерина)',
+        [11]: 'Хабаровск (Индустриальная, 1)',
+        [12]: 'Хабаровск (Индустриальная, ТЦ Универсал)',
+        [13]: 'Хабаровск (Металлистов)'
+    }
+    return literalObject[i]
 }
-)()
+console.log(manager(6))
--- parser.js
+++ parser.js
@@ -24,65 +24,97 @@ const password = 'FdnjCthutq2017';
     }
 
     for (const informationArray of json) {
+
+        const product = {}
+
         const number = informationArray[0]
-        const code = informationArray[1]
-        const name = informationArray[2]
-        const producer = informationArray[3]
+        const code = informationArray[1] 
+        product.code = code
+        const name = informationArray[2] 
+        product.name = name
+        const producer = informationArray[3] 
+        product.producer = producer 
         //3
         try {
             await page.waitForTimeout(2000)
             await page.type(selectors.searchField, code)
             await page.click(selectors.searchButton)
             
-            await page.waitForTimeout(2000)
-            await page.waitForSelector(selectors.informationField)
+            await page.waitForTimeout(3000)
             
             const freshArray = await page.$$(selectors.informationField+'> td')
+            freshArray.shift()
             freshArray.pop()
             freshArray.pop()
             
+            const storages = []
             for(i=0;i<freshArray.length; i++){
                 const element = freshArray[i]
-                if (i==0){
-                    console.log('info')
-                }
+                if (i==0){}
                 else if(i==freshArray.length-1){
                     const price = await element.evaluate(e=>e.firstElementChild.innerHTML,element)
-                    console.log(price)
+                    product.price = price
+                    console.log('price: ' + price)
                 }
                 else{
+                    function manager(i){
+                        const literalObject = {
+                            [1]: 'Артем (Фрунзе)',
+                            [2]: 'Биробиджан (Пионерская)',
+                            [3]: 'Благовещенск (Воронкова)',
+                            [4]: 'Благовещенск (Театральная)',
+                            [5]: 'Владивосток (Военное Шоссе)',
+                            [6]: 'Владивосток (Камская)',
+                            [7]: 'Владивосток (Кубанская)',
+                            [8]: 'Комсомольск-на-Амуре (Кирова)',
+                            [9]: 'Находка (Вторая)',
+                            [10]: 'Уссурийск (Чичерина)',
+                            [11]: 'Хабаровск (Индустриальная, 1)',
+                            [12]: 'Хабаровск (Индустриальная, ТЦ Универсал)',
+                            [13]: 'Хабаровск (Металлистов)'
+                        }
+                        return literalObject[i]
+                    }
+                    const storage = {
+                        location: manager(i)
+                    }
+                    
+                    
                     const transit = await element.$('.transit')
                     const count = await element.$('.amois')
-                    const tag = await element.$('.amono')
+                    const tag = await element.$('.amono')//проверка на прочерк
                     if(count){
                         const c= await page.evaluate(e=>e.innerHTML, count)
-                        console.log(c)
+                        storage.count = +c
                     }
                     else if(tag){
-                        console.log(0)
+                        storage.count = 0
                     }
                     if(transit){
                         await transit.click()
                         await page.waitForTimeout(2000)
                         const [date, count]= await page.$$eval(selectors.transitField+'> td',e=>e.map(e=>e.innerHTML))
-                        console.log(date, count)
+                        storage.transit = {date: date, count: count}
                         await page.click(selectors.transitFieldExit)
                     } 
-                    else{
-                        console.log('no transit')
-                    }
+                    storages.push(storage)
+                    console.log(storage)
                     await page.waitForTimeout(1000)
             }
                 
-            }
-            await page.click(selectors.searchField)
-            await page.keyboard.down( 'Control' );
-            await page.keyboard.press( 'A' );
-            await page.keyboard.up( 'Control' );
-            await page.keyboard.press( 'Backspace' );
-            
-            await page.waitForTimeout(1000)
-           
+        }
+
+        product.storages = storages
+        console.log(product)
+
+
+        await page.click(selectors.searchField)
+        await page.keyboard.down( 'Control' );
+        await page.keyboard.press( 'A' );
+        await page.keyboard.up( 'Control' );
+        await page.keyboard.press( 'Backspace' );
+        await page.waitForTimeout(1000)
+        
         } catch (error) {
             console.log(error)
         }
